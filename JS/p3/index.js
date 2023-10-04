6f218ff31be07c3a79998656413f1966de008af5
youdame <112458620+youdame@users.noreply.github.com>
2023-10-04 15:08:57 +0900

    JS(p3) : 이벤트 버블링 stopPropagation()

---
+++ JS/p3/index.js
@@ -0,0 +1,28 @@
+// 이벤트 버블링 (Event Bubbling)
+const content = document.querySelector('#content');
+const title = document.querySelector('#title');
+const list = document.querySelector('#list');
+const items = document.querySelectorAll('.item');
+
+content.addEventListener('click', function(e) {
+  console.log('content Event');
+  console.log(e.currentTarget);
+});
+
+title.addEventListener('click', function(e) {
+  e.stopPropagation();
+  console.log('title Event');
+  console.log(e.currentTarget);
+});
+
+list.addEventListener('click', function(e) {
+  console.log('list Event');
+  console.log(e.currentTarget);
+});
+
+for (let item of items) {
+  item.addEventListener('click', function(e) {
+    console.log('item Event');
+    console.log(e.currentTarget);
+  });
+}
